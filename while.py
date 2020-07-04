c9a52268ee6a1fa4fc5dd5a86735aa47fbc604e2
shafu <mahajanshafu@gmail.com>
2020-07-04 16:55:46 +0530

    added while.py file

---
+++ while.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 15 12:07:33 2019
+
+@author: shafu
+"""
+
+sum=0
+i=int(input("Enter value"))
+
+while i in range(0,10):
+    sum=sum+i
+    print(sum)
